3d85806164c630a65375da7adbcd1a42d7b0a5e5
croxsanchez <crox.sanchez@gmail.com>
2021-08-25 19:53:56 -0500

    Corrección 8 de prueba 1 de envío de email

--- appIva.js
+++ appIva.js
@@ -1,10 +1,10 @@
 // const sql = require('mssql');
-// const s = require('underscore.string');
+const s = require('underscore.string');
 // const formatNumber = require('simple-format-number');
 // const moment = require('moment');
 // const async_lib = require('async');
 import {sql} from "mssql";
-import {s} from "underscore.string";
+// import {s} from "underscore.string";
 import {formatNumber} from "simple-format-number";
 import {moment} from "moment";
 import {async_lib} from "async";
